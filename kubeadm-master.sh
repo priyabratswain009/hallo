42227c57ed569055740583a6f8b943f2d0949467
priyabratswain009 <86113527+priyabratswain009@users.noreply.github.com>
2024-01-21 19:02:22 +0530

    Create kubeadm-master.sh

---
+++ kubeadm-master.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+# Run the following commands on both the master and worker nodes to prepare them for kubeadm
+
+sudo apt update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo apt install docker.io -y
+
+sudo systemctl enable --now docker # enable and start in single command.
+
+# Adding GPG keys.
+curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
+
+# Add the repository to the sourcelist.
+echo 'deb https://packages.cloud.google.com/apt kubernetes-xenial main' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update 
+sudo apt install kubeadm=1.20.0-00 kubectl=1.20.0-00 kubelet=1.20.0-00 -y
+
+#only on master node
+
+sudo kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# install weave cni the 
+
+kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
+# For adding worker nodes run below 2nd command
+
+# generate token
+
+# sudo kubeadm token create --print-join-command
